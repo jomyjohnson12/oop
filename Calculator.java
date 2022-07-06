122a0b1d5d9bcbd85384884f45ea3d35f4f838ab
Jomy Johnson <95912027+jomyjohnson12@users.noreply.github.com>
2022-07-06 10:14:47 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -13,6 +13,9 @@ TextField t2=new TextField();
 TextField t3=new TextField();
 
 Button b1=new Button("Add");
+Button b2=new Button("Sub");
+Button b3=new Button("Mul");
+Button b4=new Button("Div");
 Button b5=new Button("Cancel");
 
 Calculator()
@@ -21,14 +24,22 @@ Calculator()
     l2.setBounds(50,140,100,20);
     l3.setBounds(50,180,100,20);
 
-t1.setBounds(200,100,100,20);
-t2.setBounds(200,140,100,20);
-t3.setBounds(200,180,100,20);
+t1.setBounds(250,100,100,20);
+t2.setBounds(250,140,100,20);
+t3.setBounds(250,180,100,20);
 
 b1.setBounds(50,250,50,20);
-b5.setBounds(290,250,50,20);
+b2.setBounds(130,250,50,20);
+b3.setBounds(230,250,50,20);
+b4.setBounds(330,250,50,20);
+b5.setBounds(430,250,50,20);
 
-b1.setBackground(Color.red);
+b1.setBackground(Color.green);
+b2.setBackground(Color.green);
+b3.setBackground(Color.green);
+b4.setBackground(Color.green);
+b5.setBackground(Color.red);
+
 
 f1.add(l1);
 f1.add(l2);
@@ -37,9 +48,15 @@ f1.add(t1);
 f1.add(t2);
 f1.add(t3);
 f1.add(b1);
+f1.add(b2);
+f1.add(b3);
+f1.add(b4);
 f1.add(b5);
 
 b1.addActionListener(this);
+b2.addActionListener(this);
+b3.addActionListener(this);
+b4.addActionListener(this);
 b5.addActionListener(this);
 
 f1.setLayout(null);
@@ -59,6 +76,24 @@ public void actionPerformed(ActionEvent e)
 if(e.getSource()==b1)
 {
     t3.setText(String.valueOf(n1+n2));
+
+}
+
+if(e.getSource()==b2)
+{
+    t3.setText(String.valueOf(n1-n2));
+
+}
+
+if(e.getSource()==b3)
+{
+    t3.setText(String.valueOf(n1*n2));
+
+}
+
+if(e.getSource()==b4)
+{
+    t3.setText(String.valueOf(n1/n2));
 
 }
 
